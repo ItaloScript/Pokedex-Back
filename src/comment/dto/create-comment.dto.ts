59b0946c5f324ca9ff05bf2004580ebb80b28303
italoScript <italoyoupw@gmail.com>
2022-12-05 02:01:35 -0300

    chore: swagger removed for now, since is not required in the moment

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,21 +1,16 @@
-import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNumber, IsString } from "class-validator";
 
 export class CreateCommentDto {
     
-    @ApiProperty( { description: 'The name of the comment', example:  'This is a comment' } ) 
     @IsString()
     comment: string;
 
-    @ApiProperty( { description: 'The Pokemon Unique ID', example:  1 } )
     @IsNumber()
     pokemonId: number;
 
-    @ApiProperty()
     @IsString()
     username: string;
 
-    @ApiProperty()
     @IsEmail()
     email: string;
 
